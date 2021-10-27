1f1794273e7d32a2def0ec06b1e8d52338ee9962
PeterT <joebloggsnz@gmail.com>
2021-10-27 23:27:19 +1300

    First commit

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+const moment = require('moment-timezone');
+const format = require('date-fns/format');
+
+module.exports = (function (eleventyConfig) {
+    eleventyConfig.addFilter('date', function (date, dateFormat) {
+      return format(date, dateFormat)
+    });
+    eleventyConfig.addFilter("dateFormat", function (dateIn) {
+        return moment(dateIn).tz('GMT').format('DD/MM/YYYY');
+    });
+
+    return {
+        templateFormats: ["njk", "md", "jpg", "jpeg", "png", "ico", "html", "css", "xml"]
+    };
+
+});
---
+++ _data/site.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  siteName: 'Nelson Quilters Guild',  
+  siteDescription: 'The Nelson Quilters\' Guild was established in 1983 as the Sun City Patchwork and Quilters. Since that time it has continued to grow. The Guild has 80 members and at most monthly meetings we have about 40 quilters present.',
+  siteAuthor: 'Peter Thompson',  
+  copyrightYear: (new Date()).getFullYear(),  
+  url: 'https://nelsonquiltersguild.org.nz',
+};
