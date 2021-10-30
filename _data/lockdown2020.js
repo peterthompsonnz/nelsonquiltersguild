bdc2b4fefed1b558615f50a08e61274207bad038
PeterT <joebloggsnz@gmail.com>
2021-10-30 17:34:51 +1300

    Various tweaks to do with image loading and formatting of links

--- _data/lockdown2020.js
+++ _data/lockdown2020.js
@@ -4,18 +4,18 @@
    imagesFolderName: 'lockdown2020',
    thumbnailsFolderName: 'lockdown2020/thumbs',
    data: [
-          {description: 'Alison Paton\'s finished community quilt', fileName: 'alison1.jpg'},
-          {description: 'Quilt top for Alison\'s spare bedroom - need the shops to open to buy some backing!', fileName: 'alison2.jpg'},
-          {description: 'Carla Wardrop made these two useful pouches, one for herself and one for her sister. The pattern had 4 sizes and she made the extra large size.', fileName: 'carla-wardrop1.jpg'},
-          {description: 'Carla also made two aprons from a pattern from MillyMac Supplies  using an ancient piece of fabric from her stash that had little cups and saucers.', fileName: 'carla-wardrop2.jpg'},
-          {description: 'Diane Renwick completed this community quilt. The top had been donated.', fileName: 'diane-renwick.jpg'},
-          {description: 'Eileen Gordon has been busy using up some of her stash with a Liberated Log Cabin pattern where you quilt the blocks as you go onto the batting.', fileName: 'eileen-gordon.jpg'},
-          {description: 'Geraldine Walker bought this kit at the Melbourne quilt show last year and made it up over Easter.', fileName: 'geraldine-walker.jpg'},
-          {description: 'June Mannix made these cute dolls from a panel for her two grandies, then downloaded the patterns to make some clothes.', fileName: 'june-mannix.jpg'},
-          {description: 'Leah Campbell put funky fabric on the back of these so you have a choice on how to use it depending on your mood! They were begun at a Karamea Quilt weekend a few years ago.', fileName: 'leah-campbell.jpg'},          
-          {description: 'Rosalie Johnson started this magnificent work 18 months ago. It is a Wendy Williams design of cotton and wool felt with lots of embroidery and quilting.', fileName: 'rosalie-johnson.jpg'},
-          {description: 'A Kaffe Fasset fabric cushion made by Sandra Cahill. Hand quilted in pearl cotton with red velvet on the back.', fileName: 'sandra-cahill.jpg'},
-          {description: 'Finished at last says Rosemary Mellsop who has made two of these quilts for grandchildren.', fileName: 'rosemary-mellsop1.jpg'}
+          {id: 1, description: 'Alison Paton\'s finished community quilt', fileName: 'alison1.jpg'},
+          {id: 2, description: 'Quilt top for Alison\'s spare bedroom - need the shops to open to buy some backing!', fileName: 'alison2.jpg'},
+          {id: 3, description: 'Carla Wardrop made these two useful pouches, one for herself and one for her sister. The pattern had 4 sizes and she made the extra large size.', fileName: 'carla-wardrop1.jpg'},
+          {id: 4, description: 'Carla also made two aprons from a pattern from MillyMac Supplies  using an ancient piece of fabric from her stash that had little cups and saucers.', fileName: 'carla-wardrop2.jpg'},
+          {id: 5, description: 'Diane Renwick completed this community quilt. The top had been donated.', fileName: 'diane-renwick.jpg'},
+          {id: 6, description: 'Eileen Gordon has been busy using up some of her stash with a Liberated Log Cabin pattern where you quilt the blocks as you go onto the batting.', fileName: 'eileen-gordon.jpg'},
+          {id: 7, description: 'Geraldine Walker bought this kit at the Melbourne quilt show last year and made it up over Easter.', fileName: 'geraldine-walker.jpg'},
+          {id: 8, description: 'June Mannix made these cute dolls from a panel for her two grandies, then downloaded the patterns to make some clothes.', fileName: 'june-mannix.jpg'},
+          {id: 9, description: 'Leah Campbell put funky fabric on the back of these so you have a choice on how to use it depending on your mood! They were begun at a Karamea Quilt weekend a few years ago.', fileName: 'leah-campbell.jpg'},          
+          {id: 10, description: 'Rosalie Johnson started this magnificent work 18 months ago. It is a Wendy Williams design of cotton and wool felt with lots of embroidery and quilting.', fileName: 'rosalie-johnson.jpg'},
+          {id: 11, description: 'A Kaffe Fasset fabric cushion made by Sandra Cahill. Hand quilted in pearl cotton with red velvet on the back.', fileName: 'sandra-cahill.jpg'},
+          {id: 12, description: 'Finished at last says Rosemary Mellsop who has made two of these quilts for grandchildren.', fileName: 'rosemary-mellsop1.jpg'}
          ]
 }
 
