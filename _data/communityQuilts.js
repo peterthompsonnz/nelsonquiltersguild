2547f818844548c9be439ad10e27458c8133b217
Peter Thompson <joebloggsnz@gmail.com>
2021-11-02 21:07:04 +1300

    Added community quilts page and media queries to centre thumbnails and adjust nav links spacing

---
+++ _data/communityQuilts.js
@@ -0,0 +1,16 @@
+ 'use strict'
+ 
+ module.exports = {
+   imagesFolderName: 'images',
+   thumbnailsFolderName: 'images/thumbs',
+   data: [
+          {id: 1, description: 'Various quilts', fileName: 'community-quilts1.jpg'},
+          {id: 2, description: 'Sandra\'s pillow art', fileName: 'community-quilts2.jpg'},
+          {id: 3, description: '"Ugly" qult', fileName: 'community-quilts3.jpg'},
+          {id: 4, description: 'Child\'s quilt', fileName: 'community-quilts4.jpg'},          
+          {id: 5, description: 'Garden scenes', fileName: 'community-quilts5.jpg'},
+          {id: 6, description: 'More examples', fileName: 'community-quilts6.jpg'},
+          {id: 7, description: 'Childrens quilts', fileName: 'community-quilts7.jpg'},
+          {id: 8, description: 'Leone\'s quilt (unfinished at the time)', fileName: 'community-quilts8.jpg'},          
+         ]
+}
