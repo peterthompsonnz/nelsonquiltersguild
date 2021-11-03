08c93175eba4328e7d5cb44924a91726cedca193
Peter Thompson <joebloggsnz@gmail.com>
2021-11-04 09:32:17 +1300

    Added did you know that workshop page, set colours in css to use variables, tidied up flexbox code on newsletters page so text in right column would not be cut off on smaller screens

---
+++ _data/didYouKnowThat.js
@@ -0,0 +1,47 @@
+'use strict'
+ 
+module.exports = {
+   "imagesFolderName":"images",   
+   "data":[
+            {
+               "lazyLoad":false,
+               "description":"Bronwyn demonstrating accurate measuring",
+               "fileName":"workshop1.jpg"
+            },
+            {
+               "lazyLoad":true,
+               "description":"Using Ricky's quilt to explain border lengths",
+               "fileName":"workshop2.jpg"
+            },
+            {
+               "lazyLoad":true,
+               "description":"Cutting quilt edges before attaching the binding",
+               "fileName":"workshop3.jpg"
+            },
+            {
+               "lazyLoad":true,
+               "description":"The Flying Geese pattern cutting",
+               "fileName":"workshop4.jpg"
+            },
+            {
+               "lazyLoad":true,
+               "description":"Binding tips",
+               "fileName":"workshop5.jpg"
+            },
+            {
+               "lazyLoad":true,
+               "description":"Flying Geese",
+               "fileName":"workshop6.jpg"
+            },
+            {
+               "lazyLoad":true,
+               "description":"Quarter square triangles",
+               "fileName":"workshop7.jpg"
+            },
+            {
+               "lazyLoad":true,
+               "description":"Half square and quarter square triangles",
+               "fileName":"workshop8.jpg"
+            }
+         ]
+}
