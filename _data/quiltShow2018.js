0971a3721fa29f5703b6ed3b06050fa90a2b7aa6
Peter Thompson <joebloggsnz@gmail.com>
2021-11-03 17:13:26 +1300

    Added a page for 2018 quilt show

---
+++ _data/quiltShow2018.js
@@ -0,0 +1,42 @@
+ 'use strict'
+ 
+ module.exports = {
+   imagesFolderName: 'images',
+   thumbnailsFolderName: 'images/thumbs',
+   data: [
+          {id: 1, description: 'A cat and a quilt are like bread and jam - Valerie McNaughton', fileName: 'valerie-mcnaughton.jpg'},
+          {id: 2, description: '34 Shades of Grey - Christine Pretty', fileName: 'christine-pretty.jpg'},
+          {id: 3, description: 'A Kiwi in Federation Square - Chris Tait', fileName: 'chris-tait.jpg'},
+          {id: 4, description: 'A Love of Red and White - Pat Pomphret-Brown', fileName: 'pat-pomphret-brown.jpg'},
+          {id: 5, description: 'A Day at the Beach - Chris Wensley', fileName: 'chris-wensley.jpg'},
+          {id: 6, description: 'Autumn Bounty - June Mannix', fileName: 'june-mannix.jpg'},
+          {id: 7, description: 'Buddy Boy - Daphne Willis', fileName: 'daphne-willis.jpg'},
+          
+          {id: 8, description: 'C is for - Carolyn Henry', fileName: 'carolyn-henry.jpg'},
+          {id: 8, description: 'Noah and Friends - Pat Pomfret-Brown', fileName: 'pat-pomphret-brown2.jpg'},
+          {id: 8, description: 'Village Green - Lyn McCullough', fileName: 'lyn-mccullough.jpg'},
+          {id: 8, description: 'Tulips in Disguise - Lana Wilson', fileName: 'lana-wilson.jpg'},
+          {id: 8, description: 'Triple Diamond - Lynette Tobin', fileName: 'lynette-tobin.jpg'},
+          {id: 8, description: 'Trip Around the World (Autumn) - Joan Inwood', fileName: 'joan-inwood.jpg'},
+          {id: 8, description: 'The princess and the Flag - Maij Toker', fileName: 'maij-toker.jpg'},
+          {id: 8, description: 'The Last of the 21\'s - Lana Wilson', fileName: 'lana-wilson2.jpg'},
+          {id: 8, description: 'Tesellated Leaves - June Mannix', fileName: 'june-mannix2.jpg'},
+          {id: 8, description: 'Silk Dresses - Ruth Lewis', fileName: 'ruth-lewis.jpg'},
+          {id: 8, description: 'Kimono Upcycle - Alison Paton', fileName: 'alison-paton.jpg'},
+          {id: 8, description: 'Something Fishy - June Mannix', fileName: 'june-mannix3.jpg'},
+          {id: 8, description: 'Postcard Challenge 1', fileName: 'postcard-challenge1.jpg'},
+          {id: 8, description: 'Postcard Challenge 2', fileName: 'postcard-challenge2.jpg'},
+          {id: 8, description: 'Exotic Marigold - Christine Pretty', fileName: 'christine-pretty2.jpg'},
+          {id: 8, description: 'Montana Skies - Daphne Willis', fileName: 'daphne-willis2.jpg'},
+           {id: 8, description: 'Little Hooter - Lana Wilson', fileName: 'lana-wilson3.jpg'},
+          {id: 8, description: 'Owls in Autumn - Joan Inwood', fileName: 'joan-inwood2.jpg'},
+           {id: 8, description: 'Pat and Lisa\'s Wedding - Sandra Cahil', fileName: 'sandra-cahil.jpg'},
+          {id: 8, description: 'Patterns - Gillian Dickson', fileName: 'gillian-dickson.jpg'},
+           {id: 8, description: 'Portrait of Calin - Chris Tait', fileName: 'chris-tait2.jpg'},
+          {id: 8, description: 'Patchwork Puzzle - Diane Renwick', fileName: 'diane-renwick.jpg'},
+          
+          {id: 8, description: 'Tan Square - Joan Inwood', fileName: 'joan-inwood3.jpg'},
+           {id: 8, description: 'Subcommittee - Diane Renwick', fileName: 'diane-renwick2.jpg'},
+          {id: 8, description: 'A Window on NZ - Alison Paton', fileName: 'alison-paton2.jpg'},
+         ]
+}
