11056b475352d862e7bd394a31ad70aa1b84f3bc
Peter Thompson <joebloggsnz@gmail.com>
2021-11-26 17:15:31 +1300

    Added box shadow and border-radius to images; added descriptive November meeting paragraph to the meeting's gallery page

---
+++ _data/novemberMeeting2021.js
@@ -0,0 +1,110 @@
+ 'use strict'
+ 
+ module.exports = {
+   "imagesFolderName": "images",
+   "thumbnailsFolderName": "images/thumbs",
+   "data": [         
+           {
+              "lazyLoad":true,
+              "description":"<strong>First placegetter - Marion Whyte</strong>",
+              "fileName":"first-place.jpg"
+           },
+           {
+              "lazyLoad":true,
+              "description":"<strong>Second placegetter - Steph Hazlitt-Black</strong>",
+              "fileName":"birds.jpg"
+           },
+           {
+              "lazyLoad":true,
+              "description":"<strong>Third placegetter - Steph Murch</strong>",
+              "fileName":"child-and-balloons.jpg"
+           },
+           {
+              "lazyLoad":true,
+              "description":"Looks like this caterpillar is heading off to bed",
+              "fileName":"green-and-yellow-caterpillar.jpg"
+           },
+           {
+              "lazyLoad":true,
+              "description":"A happy family of Dr Seuss characters - note the LED light",
+              "fileName":"challenge-by-alison.jpg"
+           },
+           {
+              "lazyLoad":true,
+              "description":"A blue butterfly",
+              "fileName":"blue-butterfly.jpg"
+           },
+           {
+              "lazyLoad":true,
+              "description":"A call to action!",
+              "fileName":"discombobulated.jpg"
+           },
+            {
+              "lazyLoad":true,
+              "description":"Pretty flowers and fancy fish",
+              "fileName":"fish-and-flowers.jpg"
+           },
+           {
+              "lazyLoad":true,
+              "description":"Blue flowers, spotted leaves and words",
+              "fileName":"blue-flowers.jpg"
+           },
+           {
+              "lazyLoad":true,
+              "description":"A 3D jug and geometric shapes",
+              "fileName":"jug.jpg"
+           },
+           {
+              "lazyLoad":true,
+              "description":"Decorations hanging from a line",
+              "fileName":"hanging-from-a-line.jpg"
+           },
+           {
+              "lazyLoad":true,
+              "description":"Still life with birds",
+              "fileName":"still-life.jpg"
+           },
+           {
+              "lazyLoad":true,
+              "description":"Colourful face with spikey hair",
+              "fileName":"purple-face.jpg"
+           },
+           {
+              "lazyLoad":true,
+              "description":"Flowers in a circle",
+              "fileName":"flowers-in-a-circle.jpg"
+           },
+           {
+              "lazyLoad":true,
+              "description":"Get vacinnated!",
+              "fileName":"vacinnated.jpg"
+           },
+           {
+              "lazyLoad":true,
+              "description":"Diamonds are forever",
+              "fileName":"diamond-shape.jpg"
+           },
+           {
+              "lazyLoad":true,
+              "description":"Big hair day",
+              "fileName":"big-hair.jpg"
+           },
+           {
+              "lazyLoad":true,
+              "description":"The world is a circle",
+              "fileName":"circles.jpg"
+           },
+           {
+              "lazyLoad":true,
+              "description":"The very surprised caterpillar",
+              "fileName":"surprised-caterpillar.jpg"
+           },
+           
+           
+           
+           
+           
+           
+           
+        ]
+}
