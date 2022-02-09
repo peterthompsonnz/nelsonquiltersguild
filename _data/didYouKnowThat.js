2402705a8d0403f307c6d0de8fa6680742d7f4a0
Peter Thompson <joebloggsnz@gmail.com>
2022-02-10 12:05:47 +1300

    Added width and heights to images to help with CLS

--- _data/didYouKnowThat.js
+++ _data/didYouKnowThat.js
@@ -6,41 +6,57 @@ module.exports = {
             {
                "lazyLoad":false,
                "description":"Bronwyn demonstrating accurate measuring",
+               "width": 849, 
+               "height": 600,
                "fileName":"workshop1.jpg"
             },
             {
                "lazyLoad":true,
                "description":"Using Ricky's quilt to explain border lengths",
+               "width": 552,
+               "height": 550,
                "fileName":"workshop2.jpg"
             },
             {
                "lazyLoad":true,
                "description":"Cutting quilt edges before attaching the binding",
+               "width": 886, 
+               "height": 800,
                "fileName":"workshop3.jpg"
             },
             {
                "lazyLoad":true,
                "description":"The Flying Geese pattern cutting",
+               "width": 485,
+               "height": 529,
                "fileName":"workshop4.jpg"
             },
             {
                "lazyLoad":true,
                "description":"Binding tips",
+               "width": 600, 
+               "height": 800,
                "fileName":"workshop5.jpg"
             },
             {
                "lazyLoad":true,
                "description":"Flying Geese",
+               "width": 451, 
+               "height": 600,
                "fileName":"workshop6.jpg"
             },
             {
                "lazyLoad":true,
                "description":"Quarter square triangles",
+               "width": 915,
+               "height": 789,
                "fileName":"workshop7.jpg"
             },
             {
                "lazyLoad":true,
                "description":"Half square and quarter square triangles",
+               "width": 654, 
+               "height": 800,
                "fileName":"workshop8.jpg"
             }
          ]
