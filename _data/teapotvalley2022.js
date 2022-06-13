7892194bd0681b26bd0c6d548958a312b4e2b0aa
joe bloggs <joebloggsnz@gmail.com>
2022-06-13 15:09:17 +1200

    Added a gallery page and images for June 2022 Teapot Valley Retreat

---
+++ _data/teapotvalley2022.js
@@ -0,0 +1,88 @@
+'use strict'
+
+module.exports = {
+   "imagesFolderName": "images",
+   "thumbnailsFolderName": "images/thumbs",
+   "data": [
+      {
+         "lazyLoad": false,
+         "description": "",
+         "fileName": "xmas-design.jpg"
+      },
+      {
+         "lazyLoad": true,
+         "description": "",
+         "fileName": "triangles.jpg"
+      },
+      {
+         "lazyLoad": true,
+         "description": "",
+         "fileName": "squares.jpg"
+      },
+      {
+         "lazyLoad": true,
+         "description": "",
+         "fileName": "sea-horses.jpg"
+      },
+      {
+         "lazyLoad": true,
+         "description": "",
+         "fileName": "santa-stockings.jpg"
+      },
+      {
+         "lazyLoad": true,
+         "description": "",
+         "fileName": "rosalie.jpg"
+      },
+      {
+         "lazyLoad": true,
+         "description": "",
+         "fileName": "red-white-blue-green.jpg"
+      },
+      {
+         "lazyLoad": true,
+         "description": "",
+         "fileName": "lizards.jpg"
+      },
+      {
+         "lazyLoad": true,
+         "description": "",
+         "fileName": "japanese-dress.jpg"
+      },
+      {
+         "lazyLoad": true,
+         "description": "",
+         "fileName": "flowers.jpg"
+      },
+      {
+         "lazyLoad": true,
+         "description": "",
+         "fileName": "floral.jpg"
+      },
+      {
+         "lazyLoad": true,
+         "description": "",
+         "fileName": "diamonds.jpg"
+      },
+      {
+         "lazyLoad": true,
+         "description": "",
+         "fileName": "blue-white-black.jpg"
+      },
+      {
+         "lazyLoad": true,
+         "description": "",
+         "fileName": "balloons-cars-bikes.jpg"
+      },
+      {
+         "lazyLoad": true,
+         "description": "",
+         "fileName": "apples.jpg"
+      },
+      {
+         "lazyLoad": true,
+         "description": "",
+         "fileName": "alison's-cushions.jpg"
+      }
+   ]
+}
