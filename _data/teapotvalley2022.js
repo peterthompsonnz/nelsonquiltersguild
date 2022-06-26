f24f36be74d6ea1f7edc0e9c11811d22ea636834
joe bloggs <joebloggsnz@gmail.com>
2022-06-26 14:51:00 +1200

    Added names to teapot valley images, plus image widths and heights, edited text on page for better readability

--- _data/teapotvalley2022.js
+++ _data/teapotvalley2022.js
@@ -6,82 +6,114 @@ module.exports = {
    "data": [
       {
          "lazyLoad": false,
-         "description": "",
+         "width": 900,
+         "height": 1190,
+         "description": "By Elizabeth Morris",
          "fileName": "xmas-design.jpg"
       },
       {
          "lazyLoad": true,
-         "description": "",
+         "width": 836,
+         "height": 1418,
+         "description": "By Anne Bieleski",
          "fileName": "triangles.jpg"
       },
       {
          "lazyLoad": true,
-         "description": "",
+         "width": 900,
+         "height": 450,
+         "description": "By Elizabeth Morris",
          "fileName": "squares.jpg"
       },
       {
          "lazyLoad": true,
-         "description": "",
+         "width": 900,
+         "height": 589,
+         "description": "By unknown",
          "fileName": "sea-horses.jpg"
       },
       {
          "lazyLoad": true,
-         "description": "",
+         "width": 900,
+         "height": 1457,
+         "description": "By Rosalie Johnson",
          "fileName": "santa-stockings.jpg"
       },
       {
          "lazyLoad": true,
-         "description": "",
+         "width": 888,
+         "height": 1300,
+         "description": "By Rosalie Johnson",
          "fileName": "rosalie.jpg"
       },
       {
          "lazyLoad": true,
-         "description": "",
+         "width": 900,
+         "height": 1047,
+         "description": "By Leone Burborough",
          "fileName": "red-white-blue-green.jpg"
       },
       {
          "lazyLoad": true,
-         "description": "",
+         "width": 900,
+         "height": 1162,
+         "description": "By Geraldine Walker",
          "fileName": "lizards.jpg"
       },
       {
          "lazyLoad": true,
-         "description": "",
+         "width": 900,
+         "height": 950,
+         "description": "By Anne Churcher",
          "fileName": "japanese-dress.jpg"
       },
       {
          "lazyLoad": true,
-         "description": "",
+         "width": 900,
+         "height": 1224,
+         "description": "By Lorraine Brady",
          "fileName": "flowers.jpg"
       },
       {
          "lazyLoad": true,
-         "description": "",
+         "width": 900,
+         "height": 780,
+         "description": "By Lesley Argyle",
          "fileName": "floral.jpg"
       },
       {
          "lazyLoad": true,
-         "description": "",
+         "width": 800,
+         "height": 910,
+         "description": "By June Mannix",
          "fileName": "diamonds.jpg"
       },
       {
          "lazyLoad": true,
-         "description": "",
+         "width": 900,
+         "height": 880,
+         "description": "By Ricky Unterberger",
          "fileName": "blue-white-black.jpg"
       },
       {
          "lazyLoad": true,
-         "description": "",
+         "width": 900,
+         "height": 1099,
+         "description": "Geraldine Walker",
          "fileName": "balloons-cars-bikes.jpg"
       },
       {
          "lazyLoad": true,
-         "description": "",
+         "width": 900,
+         "height": 521,
+         "description": "By Joan Inwood",
          "fileName": "apples.jpg"
       },
       {
          "lazyLoad": true,
-         "description": "",
+         "width": 737,
+         "height": 1318,
+         "description": "By Alison Paton",
          "fileName": "alison's-cushions.jpg"
       }
    ]
