834c4d34ef17cb4295c3c61fa0d10b8c7962701a
joe bloggs <joebloggsnz@gmail.com>
2023-01-30 15:57:32 +1300

    Changed contact person and warning message to be more prominent

--- _data/site.js
+++ _data/site.js
@@ -3,7 +3,7 @@
 module.exports = {
    "siteName": "Nelson (New Zealand) Quilters Guild",
    "siteDescription": "The Nelson New Zealand) Quilters' Guild was established in 1983 as the Sun City Patchwork and Quilters. Since that time it has continued to grow. The Guild has 80 members and at most monthly meetings we have about 40 quilters present.",
-   "siteAuthor": "Peter Thompson",
+   "siteAuthor": "PeterT",
    "lastModified": (new Date()).toLocaleDateString(),
    "url": "https://nelsonquiltersguild.org.nz"
 }
