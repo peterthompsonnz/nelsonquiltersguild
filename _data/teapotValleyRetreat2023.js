eb548f9622f4f22cd117564e4f70eb0807b5962d
joe bloggs <joebloggsnz@gmail.com>
2023-06-11 21:48:28 +1200

    Finished Teapot Valley page and added a CTA on homepage to encourage visitors to check out the galleries

---
+++ _data/teapotValleyRetreat2023.js
@@ -0,0 +1,93 @@
+'use strict'
+ 
+module.exports = {
+   "imagesFolderName":"images",   
+   "data":[
+            {
+               "lazyLoad":false,
+               "description":"A wide shot of the quilting room showing everyone busy",
+               "width": 960, 
+               "height": 573,
+               "fileName":"wide-shot-of-room.jpg"
+            },
+            {
+               "lazyLoad":true,
+               "description":"Rosalie doing some fine stitching",
+               "width": 960, 
+               "height": 972,
+               "fileName":"rosalie-johnson-stitching.jpg"
+            },
+            {
+               "lazyLoad":true,
+               "description":"Chris is quilting a festive scene",
+               "width": 900,
+               "height": 882,
+               "fileName":"chris-webb-and-quilt.jpg"
+            },
+            {
+               "lazyLoad":true,
+               "description":"Lots of busy ladies",
+               "width": 900, 
+               "height": 873,
+               "fileName":"group-of-quilters.jpg"
+            },
+            {
+               "lazyLoad":true,
+               "description":"The two Stephs",
+               "width": 900,
+               "height": 902,
+               "fileName":"two-stephs.jpg"
+            },
+            {
+               "lazyLoad":true,
+               "description":"Rosanne is well organised by the look of it",
+               "width": 900, 
+               "height": 757,
+               "fileName":"rosanne.jpg"
+            },
+            {
+               "lazyLoad":true,
+               "description":"Jackie's quilt has a polynesian style to it",
+               "width": 756, 
+               "height": 1029,
+               "fileName":"jackie.jpg"
+            },
+            {
+               "lazyLoad":true,
+               "description":"Lorraine O'Neil's striking pinned-out one-block-wonder quilt in the making",
+               "width": 900,
+               "height": 1012,
+               "fileName":"unknown-oneblockwonder.jpg"
+            },           
+            {
+               "lazyLoad":true,
+               "description":"Debbie Williams' work speak for itself in terms of quality and subject matter",
+               "width": 900, 
+               "height": 1111,
+               "fileName":"debbie-williams/hare-back-on.jpg"
+            },
+            {
+               "lazyLoad":true,
+               "description":"Debbie, happy in her work",
+               "width": 900, 
+               "height": 1169,
+               "fileName":"debbie-williams/debbie-at-work.jpg"
+            },
+            {
+               "lazyLoad":true,
+               "description":"The strawberries look good enough to eat",
+               "width": 900, 
+               "height": 1074,
+               "fileName":"debbie-williams/hare-and-strawberries.jpg"
+            },
+            {
+               "lazyLoad":true,
+               "description":"A happy fox and a happy leveret",
+               "width": 900, 
+               "height": 1200,
+               "fileName":"debbie-williams/fox.jpg"
+            },           
+ 
+            
+         ]
+}
