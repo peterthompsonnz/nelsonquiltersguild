950fda24eca37fd3935d6095517988ebb7a0b319
joe bloggs <joebloggsnz@gmail.com>
2023-09-12 16:57:43 +1200

    Added Whims workshop page

---
+++ _data/anneMurchisonWorkshop2023.js
@@ -0,0 +1,98 @@
+'use strict'
+
+module.exports = {
+   "imagesFolderName": "images",
+   "data": [{
+      "lazyLoad": false,
+      "description": "Dragon Flies: An example of a finished quilt",
+      "width": 540,
+      "height": 725,
+      "fileName": "dragonflies.jpg"
+   },
+   {
+      "lazyLoad": true,
+      "description": "There was a good turnout at the workshop",
+      "width": 540,
+      "height": 449,
+      "fileName": "group-shot.jpg"
+   },
+   {
+      "lazyLoad": true,
+      "description": "Anne Murchison",
+      "width": 290,
+      "height": 544,
+      "fileName": "anne-murchison.jpg"
+   },
+   {
+      "lazyLoad": true,
+      "description": "Maggie and Kim busy at work",
+      "width": 540,
+      "height": 433,
+      "fileName": "maggie-and-kim.jpg"
+   },
+   {
+      "lazyLoad": true,
+      "description": "Lorraine having fun",
+      "width": 540,
+      "height": 494,
+      "fileName": "lorraine.jpg"
+   },
+   {
+      "lazyLoad": true,
+      "description": "Sandy ironing the back of her sewn together squares",
+      "width": 540,
+      "height": 546,
+      "fileName": "sandy.jpg"
+   },
+   {
+      "lazyLoad": true,
+      "description": "A closer look at Sandy's work",
+      "width": 540,
+      "height": 492,
+      "fileName": "sandy2.jpg"
+   },
+   {
+      "lazyLoad": true,
+      "description": "Sandra at work",
+      "width": 540,
+      "height": 464,
+      "fileName": "sandra.jpg"
+   },
+   {
+      "lazyLoad": true,
+      "description": "Alison and Roseanne discussing how to arrange the squares for best effect",
+      "width": 540,
+      "height": 565,
+      "fileName": "alison-and-roseanne.jpg"
+   }, 
+   {
+      "lazyLoad": true,
+      "description": "Steph HB arranging her squares",
+      "width": 750,
+      "height": 490,
+      "fileName": "steph-hb1.jpg"
+   }, 
+   {
+      "lazyLoad": true,
+      "description": "A closer view",
+      "width": 640,
+      "height": 383,
+      "fileName": "steph-hb.jpg"
+   }, 
+   {
+      "lazyLoad": true,
+      "description": "Rosalie",
+      "width": 540,
+      "height": 562,
+      "fileName": "rosalie.jpg"
+   }, 
+   {
+      "lazyLoad": true,
+      "description": "Steph and Eilleen view Eilleen's work on the phone",
+      "width": 540,
+      "height": 783,
+      "fileName": "steph-and-eilleen.jpg"
+   },
+
+   ]
+}
